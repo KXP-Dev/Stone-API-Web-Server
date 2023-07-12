c297eead97c91f7dd9c4e3cb895b5beae49f0236
The-Programming-Mango <kimperez95@outlook.com>
2023-07-12 14:19:32 +1000

    Create employee login route

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -4,6 +4,7 @@ from models.employee import Employee, employee_schema, employees_schema
 from flask_jwt_extended import create_access_token
 from sqlalchemy.exc import IntegrityError
 from psycopg2 import errorcodes
+from datetime import timedelta
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -28,3 +29,16 @@ def auth_register():
             return { 'error': 'Email address already in use' }, 409
         if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
             return { 'error': f'The {err.orig.diag.column_name} is required' }, 409
+        
+@auth_bp.route('/login', methods=['POST'])
+def auth_login():
+    body_data = request.get_json()
+    # Find the user by email address
+    stmt = db.select(Employee).filter_by(employee_email=body_data.get('employee_email'))
+    employee = db.session.scalar(stmt)
+    # If employee exists and password is correct
+    if employee and bcrypt.check_password_hash(employee.password, body_data.get('password')):
+        token = create_access_token(identity=str(employee.employee_id), expires_delta=timedelta(days=1))
+        return { 'employee_email': employee.employee_email, 'token': token, 'is_admin': employee.is_admin }
+    else:
+        return { 'error': 'Invalid email or password' }, 401
