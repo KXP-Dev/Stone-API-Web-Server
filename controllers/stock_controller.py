37a565f9084a103259eda09cf77d98d5022732df
The-Programming-Mango <kimperez95@outlook.com>
2023-07-27 16:03:43 +1000

    Commit 3 Update Stock

--- controllers/stock_controller.py
+++ controllers/stock_controller.py
@@ -40,3 +40,31 @@ def delete_inventory_item():
     db.session.commit()
 
     return jsonify({'message': 'Inventory item deleted successfully'}), 200
+
+@inventory_bp.route('/update', methods=['PUT'])
+def update_inventory_item():
+    data = request.get_json()
+    product_id = data.get('product_id')
+    product_name = data.get('product_name')
+    quantity = data.get('quantity')
+    unit_price = data.get('unit_price')
+
+    if not product_id:
+        return jsonify({'message': 'Missing required field: product_id'}), 400
+
+    item_to_update = Stock.query.get(product_id)
+    if not item_to_update:
+        return jsonify({'message': 'Inventory item not found'}), 404
+
+    # Update the item's fields if provided in the request
+    if product_name:
+        item_to_update.product_name = product_name
+    if quantity:
+        item_to_update.quantity = quantity
+    if unit_price:
+        item_to_update.unit_price = unit_price
+
+    db.session.commit()
+
+    serialized_item = stock_schema.dump(item_to_update)
+    return jsonify({'message': 'Inventory item updated successfully', 'inventory_item': serialized_item}), 200
